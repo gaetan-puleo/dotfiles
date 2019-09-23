0c1e735a91e3e8986e94934d4613dc30fe079e1f
gaetan <gaetan.puleo.dev@gmail.com>
2019-09-23 09:12:55 +0200

    add git symlink to install

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# requirements
+cp -s ~/.dotfiles/.gitconfig.dist ~/.dotfiles/.gitconfig
+
 # symlinks
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.config/compton.conf ~/.config/compton.conf
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
