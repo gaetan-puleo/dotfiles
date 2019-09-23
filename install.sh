008d424e6812921b654d7406c703d1375f274503
Gaetan Puleo <gaetan.puleo@gmail.com>
2019-09-24 01:18:56 +0200

    add fonts symlink

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-dotfiles=".dotfiles"
+dotfiles="dotfiles"
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install feh i3-wm i3lock curl git dunst lxterminal xfce4-power-manager lxappearence compton rofi dunst wget chromium-browser blueman
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y zsh feh i3-wm i3lock curl git dunst lxterminal xfce4-power-manager lxappearance compton rofi dunst wget chromium-browser blueman
 
 #polybar
-sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
-
+sudo apt install -y build-essential cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
 git clone https://github.com/jaagr/polybar.git
 
 cd polybar && ./build.sh
@@ -16,10 +15,10 @@ rm -r polybar
 
 #neovim
 
-sudo apt-get install python-dev python-pip python3-dev python3-pip
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt-get update -y
+sudo apt-get install -y neovim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs
     \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -30,10 +29,10 @@ nvim +'PlugInstall --sync' +qa
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 # requirements
-cp -s ~/$dotfiles/.gitconfig.dist ~/.dotfiles/.gitconfig
+cp -s ~/$dotfiles/.gitconfig.dist ~/$dotfiles/.gitconfig
 
 # symlinks
-# ln -s ~/$dotfiles/.icons ~/.icons
+ln -s ~/$dotfiles/.fonts ~/.fonts
 ln -s ~/$dotfiles/.config/compton.conf ~/.config/compton.conf
 ln -s ~/$dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/$dotfiles/.config/dunst ~/.config/dunst
