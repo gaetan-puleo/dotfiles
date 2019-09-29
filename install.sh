3d90b378091778e4f57e9a1750b5dd81578f7774
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2019-09-29 23:31:47 +0200

    fix symlinks

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ chsh -s /bin/bash
 
 cd ./scripts
 
-./aptinstalls.sh
-./programs.sh
-./symlinks.sh
-./clean-after-install.sh
+# ./aptinstalls.sh
+# ./programs.sh
+bash ./symlinks.sh
+# ./clean-after-install.sh
 
 figlet "... and we're back!" | lolcat
--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -2,12 +2,36 @@
 
 dotfiles="dotfiles"
 
-ln -s ~/$dotfiles/fonts ~/.fonts
-ln -s ~/$dotfiles/config/zsh/.zshrc ~/.zshrc
-ln -s ~/$dotfiles/config/compton.conf ~/.config/compton.conf
-ln -s ~/$dotfiles/config/dunst ~/.config/dunst
-ln -s ~/$dotfiles/config/i3 ~/.config/i3
-ln -s ~/$dotfiles/config/rofi ~/.config/rofi
-ln -s ~/$dotfiles/config/polybar ~/.config/polybar
-ln -s ~/$dotfiles/config/nvim ~/.config/nvim
-ln -s ~/$dotfiles/config/lxterminal ~/.config/lxterminal
+function linkDotfile {
+  dest="${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -L ${2} ]; then
+    # Existing symlink
+    echo "Removing existing symlink: ${2}"
+    rm ${dest}
+
+  elif [ -f "${2}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${2}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${2}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${2}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${2}"
+  ln -sfn ${1} ${2}
+}
+
+linkDotfile ~/$dotfiles/fonts ~/.fonts
+linkDotfile ~/$dotfiles/config/zsh/.zshrc ~/.zshrc
+linkDotfile ~/$dotfiles/config/compton.conf ~/.config/compton.conf
+linkDotfile ~/$dotfiles/config/dunst ~/.config/dunst
+linkDotfile ~/$dotfiles/config/i3 ~/.config/i3
+linkDotfile ~/$dotfiles/config/rofi ~/.config/rofi
+linkDotfile ~/$dotfiles/config/polybar ~/.config/polybar
+linkDotfile ~/$dotfiles/config/nvim ~/.config/nvim
+linkDotfile ~/$dotfiles/config/lxterminal ~/.config/lxterminal
