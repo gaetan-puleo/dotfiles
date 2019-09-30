d2d2547ed5dd2a58304698717c884ba438dba9e2
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2019-09-30 02:15:40 +0200

    fix polybar and ripgrep install

--- scripts/programs/ripgrep.sh
+++ scripts/programs/ripgrep.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb > ~/dotfiles/tmp/ripgrep.deb
-sudo dpkg -i  ~/dotfiles/tmp/ripgrep.deb
-
+(cd ~/dotfiles/tmp/; curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb)
+sudo dpkg -i  ~/dotfiles/tmp/ripgrep_11.0.2_amd64.deb
