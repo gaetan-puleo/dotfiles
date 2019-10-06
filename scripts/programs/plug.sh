caef36874aa010d5def0436cb281bb6c2300b73e
gaetan puleo <gaetan.puleo.dev@gmail.com>
2019-10-06 17:43:16 +0200

    update plug script

--- scripts/programs/plug.sh
+++ scripts/programs/plug.sh
@@ -2,4 +2,6 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+source ~/.bashrc
+
 nvim +'PlugInstall --sync' +qa
