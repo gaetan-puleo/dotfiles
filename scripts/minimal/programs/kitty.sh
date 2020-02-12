da91351875b19716038f98d551f07d9d921cabc3
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2020-02-12 23:24:11 +0100

    add multiple entries

--- scripts/minimal/programs/kitty.sh
+++ scripts/minimal/programs/kitty.sh
@@ -8,3 +8,7 @@ ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 # Update the path to the kitty icon in the kitty.desktop file
 sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator ~/.local/bin/kitty 50
+
+sudo update-alternatives --config x-terminal-emulator
+
