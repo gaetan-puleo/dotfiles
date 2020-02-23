8e084f8acaa15e3cf597999f29473aaf5d027785
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2020-02-24 00:59:17 +0100

    fix screenshot

--- rofi/.config/rofi/scripts/scrot.sh
+++ rofi/.config/rofi/scripts/scrot.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-
+case1="  Capture the selected area"
+case2="  Capture the entire screen"
+case3="  Capture the active window"
 rofi_command="rofi -theme themes/layout-scrot.rasi"
 
 # Each of the icon is a selectable element
-options=$'  Capture the selected area \n  Capture the entire screen \n  Capture the active window'
+options=$case1$'\n'$case2$'\n'$case3
 
 # chosen="$(echo "$options" | $rofi_command -font "Fira Code Nerd Font" -dmenu)"
 chosen="$(echo "$options" | $rofi_command -dmenu)"
 case $chosen in
-    ) # Scrot a rectangular area
-        maim -s $HOME/Pictures/$(date +%s)-screenshot.png
+    $case1) # Scrot a rectangular area
+        sleep 1; maim -s $HOME/Pictures/$(date +%s)-screenshot.png
         ;;
-    ) # Scrot the whole screen
+    $case2) # Scrot the whole screen
         sleep 1; maim $HOME/Pictures/$(date +%s)-screenshot.png
         ;;
-    ) # Scrot the active window
+    $case3) # Scrot the active window
         sleep 1; maim -i $(xdotool getactivewindow) $HOME/Pictures/$(date +%s)-screenshot.png
         ;;
 esac
