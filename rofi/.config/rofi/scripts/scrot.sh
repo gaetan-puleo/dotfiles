7727f830ed089d3629ed28caffe16ab04d46e7aa
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2020-02-24 01:11:47 +0100

    improve screenshot scripts

--- rofi/.config/rofi/scripts/scrot.sh
+++ rofi/.config/rofi/scripts/scrot.sh
@@ -11,13 +11,21 @@ options=$case1$'\n'$case2$'\n'$case3
 chosen="$(echo "$options" | $rofi_command -dmenu)"
 case $chosen in
     $case1) # Scrot a rectangular area
-        sleep 1; maim -s $HOME/Pictures/$(date +%s)-screenshot.png
+        NAME=$HOME/Pictures/$(date +%s)-screenshot.png;
+        maim -s $NAME;
+        notify-send 'Picture taken!' "It was saved at $NAME" --icon=$NAME;
         ;;
     $case2) # Scrot the whole screen
-        sleep 1; maim $HOME/Pictures/$(date +%s)-screenshot.png
+        sleep 1; 
+        NAME=$HOME/Pictures/$(date +%s)-screenshot.png;
+        maim $NAME;
+        notify-send 'Picture taken!' "It was saved at $NAME" --icon=$NAME;
         ;;
     $case3) # Scrot the active window
-        sleep 1; maim -i $(xdotool getactivewindow) $HOME/Pictures/$(date +%s)-screenshot.png
+        sleep 1;
+        NAME=$HOME/Pictures/$(date +%s)-screenshot.png;
+        maim -i $(xdotool getactivewindow) $NAME;
+        notify-send 'Picture taken!' "It was saved at $NAME" --icon=$NAME;
         ;;
 esac
 
