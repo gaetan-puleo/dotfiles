f5ff7347d6ba2c59a7fb5c2ab0e3185ddd73c19f
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-05-14 13:11:21 +0200

    feat: add bootstrap script

---
+++ bootstrap.bash
@@ -0,0 +1,7 @@
+#/bin/bash
+
+source "$(dirname "$0")/git/install.bash"
+source "$(dirname "$0")/kitty/install.bash"
+source "$(dirname "$0")/fonts/install.bash"
+source "$(dirname "$0")/nvim/install.bash"
+source "$(dirname "$0")/zsh/install.bash"
