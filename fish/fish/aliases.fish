4c26fe1645691437cb037663afd1e87b52a61477
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-06-10 21:20:27 +0200

    feat(fish): add md alias

--- fish/fish/aliases.fish
+++ fish/fish/aliases.fish
@@ -1,8 +1,11 @@
 alias vim="nvim"
 alias nuke="rm -rf"
+alias md="mkdir"
 
 # Update
 alias install="sudo apt install -y"
 alias update="sudo apt update -y && sudo apt upgrade -y"
 
+# git
 alias git="g"
+
