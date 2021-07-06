4ecf785d5bef7439ea38ad2767a5089c3378e06a
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-07-06 13:35:06 +0200

    fix: tmux install script

--- tmux/install.bash
+++ tmux/install.bash
@@ -1,9 +1,5 @@
 CURR_PATH=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
-cd 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-./configure && make
-
 # create a symlink
 bash "${CURR_PATH}/symlink.bash"
 
