f85260d221889277a49f8d1fb2a4ec5e41ec0e61
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-07-14 18:00:25 +0200

    fix: remove tailwindcss

--- node/install.bash
+++ node/install.bash
@@ -18,4 +18,3 @@ fish -C "npm install -g tsun \
 	bash-language-server && exit
 "
 
-fish -C 'curl -L -o tailwindcss-intellisense.vsix https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v0.6.8/vscode-tailwindcss-0.6.8.vsix && unzip tailwindcss-intellisense.vsix -d tailwindcss-intellisensei && echo "#\!/usr/bin/env node\n$(cat tailwindcss-intellisense/extension/dist/server/tailwindServer.js)" > tailwindcss-language-server && chmod +x tailwindcss-language-server" && exit'
--- nvim/nvim/lua/plugins/lsp-config.lua
+++ nvim/nvim/lua/plugins/lsp-config.lua
@@ -65,7 +65,7 @@ end
 
 
 local function setup_servers()
-	local servers = {'typescript', 'bash', 'json', 'css', 'graphql', 'html', 'tailwindcss', 'diagnosticls'}
+	local servers = {'typescript', 'bash', 'json', 'css', 'graphql', 'html', 'diagnosticls'}
 
   for _, server in pairs(servers) do
     local config = make_config()
