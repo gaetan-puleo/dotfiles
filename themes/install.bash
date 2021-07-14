bd81d92acb452ced02053daccfdc680a06d0a0e9
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-07-14 18:21:03 +0200

    fix: use juno mirage

--- themes/install.bash
+++ themes/install.bash
@@ -3,6 +3,6 @@ CURR_PATH=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
 bash "${CURR_PATH}/symlink.bash"
 
-gsettings set org.gnome.desktop.interface gtk-theme Juno
-gsettings set org.gnome.desktop.wm.preferences theme Juno
+gsettings set org.gnome.desktop.interface gtk-theme Juno-mirage
+gsettings set org.gnome.desktop.wm.preferences theme Juno-mirage
 
