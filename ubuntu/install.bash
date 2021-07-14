c9411a3cdea21b8ebca1c890bafc2d23b9010a78
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-07-14 16:49:13 +0200

    feat: make apt-fast install faster

--- ubuntu/install.bash
+++ ubuntu/install.bash
@@ -16,7 +16,8 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 28
 # Install packages
 
 # add apt fast
-sudo apt-add-repository ppa:apt-fast/stable -y && sudo apt-get update && sudo apt-get -y install apt-fast 
+sudo apt-add-repository ppa:apt-fast/stable -y < /dev/null && sudo apt-get update && echo debconf apt-fast/maxdownloads string 16 | sudo debconf-set-selections && echo debconf apt-fast/dlflag boolean true | sudo debconf-set-selections && echo debconf apt-fast/aptmanager string apt-get | sudo debconf-set-selections && sudo apt-get -y install apt-fast
+
 # neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
