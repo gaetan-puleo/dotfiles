6cd74da8b5b6baa86c65abd8beb9261865838ddd
gpuleo <gpuleo@synthesio.com>
2021-07-21 00:36:44 +0200

    feat(tmux): auto open tmux

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -7,3 +7,9 @@ set -x MANPAGER "nvim -c 'set ft=man' -"
 # sources
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/tokyonight.fish
+
+if not set -q TMUX
+    set -g TMUX tmux new-session -d -s 0
+    eval $TMUX
+    tmux attach-session -d -t 0
+end
