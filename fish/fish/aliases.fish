8898f26853c727278dcb2fcfb6bc5c39802a7175
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-10-30 13:00:50 +0200

    feat: add tmate

--- fish/fish/aliases.fish
+++ fish/fish/aliases.fish
@@ -5,9 +5,9 @@ alias md="mkdir"
 # Update
 alias install="sudo apt install -y"
 alias update="sudo apt update -y && sudo apt upgrade -y"
-
+alias pair-url="tmate display -p '#{tmate_web}'"
+alias pair-ssh="tmate display -p '#{tmate_ssh}'"
 # git
 function g
-	git $argv
+    git $argv
 end
-
