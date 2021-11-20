5f66d3cfb0797e2e9ae67574378674e2b07fb6ee
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-11-20 22:27:19 +0100

    feat(neovim): better diagnostic message

--- nvim/nvim/lua/plugins/lsp-config.lua
+++ nvim/nvim/lua/plugins/lsp-config.lua
@@ -8,13 +8,17 @@ local configs = require'lspconfig/configs'
 -- local servers = {'tsserver', 'cssls' , 'jsonls', 'graphql'}
 
 
--- vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
---   virtual_text = false,
---   signs = true,
---   underline = true,
---   update_in_insert = false,
--- })
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+})
 
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})]]
 
 -- You will likely want to reduce updatetime which affects CursorHold
 -- note: this setting is global and should be set only once
--- nvim/nvim/lua/plugins/trouble.lua
+++ nvim/nvim/lua/plugins/trouble.lua
@@ -1,4 +1,6 @@
 require("trouble").setup {
+  auto_open = true,
+	use_lsp_diagnostic_signs = true,
 	-- your configuration comes here
 	-- or leave it empty to use the default settings
 	-- refer to the configuration section below
