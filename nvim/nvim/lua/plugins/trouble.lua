e3b19e127c6519bafaae321703430516321b12c2
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2021-12-26 15:24:39 +0100

    revert: trouble auto

--- nvim/nvim/lua/plugins/trouble.lua
+++ nvim/nvim/lua/plugins/trouble.lua
@@ -1,5 +1,5 @@
 require("trouble").setup {
-  auto_open = true,
+  auto_open = false,
 	use_diagnostic_signs = true,
 	-- your configuration comes here
 	-- or leave it empty to use the default settings
