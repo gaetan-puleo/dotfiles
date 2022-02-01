de6eae6907ff7e03949e9ca3a7de897117421576
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2022-02-01 01:14:46 +0100

    feat(neovim): add basic plugins

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -16,123 +16,141 @@ vim.api.nvim_exec([[
 
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- packager for neovim
-  use 'folke/tokyonight.nvim' -- Theme
-  use 'antoinemadec/FixCursorHold.nvim'
-  use 'nvim-lua/popup.nvim' 
-  use 'nvim-lua/plenary.nvim'
-  use('sbdchd/neoformat') -- Formatter
-  use 'djoshea/vim-autoread' -- Autoread file
-  use 'RishabhRD/popfix'
+  use {'folke/tokyonight.nvim', commit = '8223c970677e4d88c9b6b6d81bda23daf11062bb'} -- Theme
+
+  use {'antoinemadec/FixCursorHold.nvim', commit = '0e4e22d21975da60b0fd2d302285b3b603f9f71e'}
+  -- use 'nvim-lua/popup.nvim' 
+  use {'nvim-lua/plenary.nvim', commit = '563d9f6d083f0514548f2ac4ad1888326d0a1c66'}
+  -- use('sbdchd/neoformat') -- Formatter
+  -- use 'djoshea/vim-autoread' -- Autoread file
+  -- use 'RishabhRD/popfix'
 
   -- cheatsheets
-  use {'folke/which-key.nvim'}
+  -- use {'folke/which-key.nvim'}
 
   -- nvim tree
-  use('kyazdani42/nvim-tree.lua')
-
+  use {
+    'kyazdani42/nvim-tree.lua',
+    commit = '2dfed89af7724f9e71d2fdbe3cde791a93e9b9e0'
+  }
+  use {
+    'lewis6991/gitsigns.nvim',
+    commit = '2fa3716bc4690da96d885becdf9988603f0756f0'
+  }
   -- comments
-	use {'tpope/vim-commentary'}
+	use {'tpope/vim-commentary', commit = '627308e30639be3e2d5402808ce18690557e8292'}
 
   -- test runners manager
-  use {'janko/vim-test'}
+  -- use {'janko/vim-test'}
 
   
   -- completion
-  use('hrsh7th/nvim-compe')
-  use('onsails/lspkind-nvim')
+  use({ 'hrsh7th/nvim-cmp', commit = 'd6838996da8dc9fe4697632d2abf34086ebab427' })
+  use({ 'hrsh7th/cmp-buffer', commit = 'f83773e2f433a923997c5faad7ea689ec24d1785' })
+  use({ 'hrsh7th/cmp-nvim-lsp' , commit = 'ebdfc204afb87f15ce3d3d3f5df0b8181443b5ba' })
+  use({ 'onsails/lspkind-nvim', commit = 'f0d1552890e384f15b47ea88bd1b8a077cddc24a' })
+  use({ 'saadparwaiz1/cmp_luasnip', commit = 'd6f837f4e8fe48eeae288e638691b91b97d1737f' })
+  use { 'L3MON4D3/LuaSnip', commit = '35322c97b041542f95c85e87a8215892ea4137d5' }
 
+  -- lsp
+  use({ 'neovim/nvim-lspconfig', commit = 'e7df7ecae0b0d2f997ea65e951ddbe98ca3e154b'})
+  use {'jose-elias-alvarez/nvim-lsp-ts-utils', commit = '64d233a8859e27139c55472248147114e0be1652' }
   -- statusline
-  use('hoob3rt/lualine.nvim')
+  -- use('hoob3rt/lualine.nvim')
 
 	-- graphql
-	use('jparise/vim-graphql')
+	-- use('jparise/vim-graphql')
   -- front page
-  use('glepnir/dashboard-nvim')
+  -- use('glepnir/dashboard-nvim')
 
   -- bufferline
-  use('akinsho/nvim-bufferline.lua')
+  -- use('akinsho/nvim-bufferline.lua')
 
 	-- icons
-	use('kyazdani42/nvim-web-devicons')
+	use {'kyazdani42/nvim-web-devicons', 
+    commit = '634e26818f2bea9161b7efa76735746838971824'
+  }
+
 
   -- better nodejs go to file
-  use('PsychoLlama/further.vim')
+  -- use('PsychoLlama/further.vim')
 
-  use('tpope/vim-dispatch')
+  -- use('tpope/vim-dispatch')
 
   -- treesitter
-  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-  use('JoosepAlviste/nvim-ts-context-commentstring')
-  use 'windwp/nvim-ts-autotag'
-	use 'nvim-treesitter/nvim-treesitter-refactor'
+  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', commit = 'efedf3510aa85f89a687dd1e29eab6f4677fa17e'})
+  use({'JoosepAlviste/nvim-ts-context-commentstring', commit = '097df33c9ef5bbd3828105e4bee99965b758dc3f'})
+  use({'windwp/nvim-ts-autotag', commit = '32bc46ee8b21f88f87d97b976ae6674595b311b5'})
+	use({'nvim-treesitter/nvim-treesitter-refactor', commit = '0dc8069641226904f9757de786a6ab2273eb73ea'})
 
   -- check color preview
-  use 'norcalli/nvim-colorizer.lua'
+  -- use 'norcalli/nvim-colorizer.lua'
 
   -- git gutter
-  use('lewis6991/gitsigns.nvim')
+  -- use({'tanvirtin/vgit.nvim', commit = '9ffd228466782d5bdcaedd8927f6327c4a14f665' })
 
 	-- snap (grep in files)
-	use { 'camspiers/snap' }
+	-- use { 'camspiers/snap' }
 
-  -- nvim lsp
-	use {'neovim/nvim-lspconfig'}
-	use {'jose-elias-alvarez/nvim-lsp-ts-utils'}
+
+
 
 	-- git wrapper
-  use('tpope/vim-fugitive')
+  -- use('tpope/vim-fugitive')
  
   -- use('glepnir/lspsaga.nvim')
-	use { 'tami5/lspsaga.nvim', branch = 'nvim51' }
-  use('christoomey/vim-tmux-navigator')
+	-- use { 'tami5/lspsaga.nvim', branch = 'nvim51' }
+  use({'christoomey/vim-tmux-navigator', commit = '9ca5bfe5bd274051b5dd796cc150348afc993b80'})
 
 	-- finder
- 	use 'nvim-telescope/telescope.nvim'
+ 	use {'nvim-telescope/telescope.nvim', commit = 'f262e7d56d37625613c5de0df5a933cccacf13c5'}
 
 	-- enable command like :32 to go to line 32
-	use 'nacro90/numb.nvim'
+	-- use 'nacro90/numb.nvim'
 
 	-- focus only current code
-	use 'folke/twilight.nvim'
+	-- use 'folke/twilight.nvim'
 	
 	-- focus current buffer
-	use 'folke/zen-mode.nvim'
+	-- use 'folke/zen-mode.nvim'
 	-- find in text
-	use 'NTBBloodbath/color-converter.nvim'
-	use 'tpope/vim-unimpaired'
+	-- use 'NTBBloodbath/color-converter.nvim'
+	-- use 'tpope/vim-unimpaired'
 
-	use 'ggandor/lightspeed.nvim'
-	use "petertriho/nvim-scrollbar"
+	-- use 'ggandor/lightspeed.nvim'
+	-- use "petertriho/nvim-scrollbar"
 	-- use 'dyng/ctrlsf.vim'
 	-- nvim window
-	use 'https://gitlab.com/yorickpeterse/nvim-window.git'
+	-- use 'https://gitlab.com/yorickpeterse/nvim-window.git'
 
-	use 'folke/trouble.nvim'
+	-- use 'folke/trouble.nvim'
 end)
 
-require('plugins/bufferline')
-require('plugins/compe')
+-- require('plugins/bufferline')
+-- require('plugins/compe')
+require('plugins/comp')
 require('plugins/commentary')
-require('plugins/dashboard')
+-- require('plugins/dashboard')
 require('plugins/gitsigns')
 require('plugins/lsp-config') 
 require('plugins/lspkind')
-require('plugins/lualine')
-require('plugins/lspsaga')
-require('plugins/neoformat') 
-require('plugins/numb') 
-require('plugins/nvim-colorizer') 
+-- require('plugins/lualine')
+-- require('plugins/lspsaga')
+-- require('plugins/neoformat') 
+-- require('plugins/numb') 
+-- require('plugins/nvim-colorizer') 
 require('plugins/nvim-tree') 
-require('plugins/nvim-window') 
-require('plugins/snap')
-require('plugins/scrollbar')
+-- require('plugins/vgit') 
+-- require('plugins/nvim-window') 
+-- require('plugins/snap')
+-- require('plugins/scrollbar')
 require('plugins/telescope')
 require('plugins/tmux-navigator')
 require('plugins/treesitter')
-require('plugins/twilight')
-require('plugins/trouble')
-require('plugins/vim-test')
-require('plugins/which-key-nvim')
-require('plugins/zen-mode')
+-- require('plugins/twilight')
+-- require('plugins/trouble')
+-- require('plugins/vim-test')
+-- require('plugins/which-key-nvim')
+-- require('plugins/zen-mode')
 
 vim.cmd[[colorscheme tokyonight]]
