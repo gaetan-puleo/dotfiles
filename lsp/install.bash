7f07f4e4502db7aab882d9045ce86921990801de
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2022-03-04 10:16:44 +0100

    refactor: update lps and yarn install scripts

--- lsp/install.bash
+++ lsp/install.bash
@@ -1,18 +1,15 @@
-fish -C "npm install -g tsun \
-	vscode-html-languageserver-bin \
-	vscode-css-languageserver-bin \
-	typescript \
-	typescript-language-server \
-	vscode-json-languageserver \
-	diagnostic-languageserver \
-	eslint_d yarn \
-	graphql-language-service-cli \
-	bash-language-server && exit
-"
+# typescript
+fish -C "npm install -g typescript typescript-language-server"
 
+# tailwindcss
+fish -C "npm install -g @tailwindcss/language-server"
 
-# add tailwindServer
-# curl -L -o tailwindcss-intellisense.vsix https://github.com/tailwindlabs/tailwindcss-intellisense/releases/download/v0.6.8/vscode-tailwindcss-0.6.8.vsix
-# unzip tailwindcss-intellisense.vsix -d tailwindcss-intellisense
-# echo "#\!/usr/bin/env node\n$(cat tailwindcss-intellisense/extension/dist/server/tailwindServer.js)" > tailwindcss-language-server
-# chmod +x tailwindcss-language-server
+# HTML - CSS - JSON
+fish -C "npm install -g vscode-langservers-extracted"
+
+# eslint_d
+fish -C "npm install -g eslint_d"
+
+# bash
+fish -C "npm install -g bash-language-server"
+
--- node/install.bash
+++ node/install.bash
@@ -4,4 +4,6 @@ fish -c "nvm install 14"
 
 fish -c "nvm use 14"
 
-fish -c "set --universal nvm_default_version v14"
+fish -c "set --universal nvm_default_version v16"
+
+fish -c "npm i -g yarn"
