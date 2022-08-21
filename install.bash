b312e259a2c9af5c79b40f5b94b363d4f927fad1
the_wild_penguin <gaetan.puleo.dev@gmail.com>
2022-08-21 21:19:19 +0200

    feat: update install script

--- install.bash
+++ install.bash
@@ -28,6 +28,7 @@ nix-env -iA nixgl.auto.nixGLDefault   # or replace `nixGLDefault` with your desi
 echo "install stow and git" 
 nix-env -iA nixpkgs.stow
 nix-env -iA nixpkgs.git
+nix-env -iA nixpkgs.fish
 
 # clone repo
 echo "Clone dotfiles"
@@ -45,30 +46,18 @@ rm ~/.bashrc
 echo "symlink everything"
 bash ./symlink.bash all
 
-echo "uninstall git and stow"
-nix-env --uninstall git
-nix-env --uninstall stow
-
-echo "Apply home-manager config"
-home-manager switch
-
-# echo "Set Fish a default shell"
-# sudo chsh $USER -s $(which fish)
-
-# echo "Source bashrc"
-# . ~/.bashrc
-
-# source nix
-# . ~/.nix-profile/etc/profile.d/nix.sh
-bash
-
 echo "Install fisher"
-
 fish -c "cd; curl -sL https://git.io/fisher | source && fisher update"
 
 echo "Install packer"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# echo "Source bashrc"
-# . ~/.bashrc
+echo "uninstall git and stow"
+nix-env --uninstall git
+nix-env --uninstall stow
+nix-env --uninstall fish
+
+echo "Apply home-manager config"
+home-manager switch
+
