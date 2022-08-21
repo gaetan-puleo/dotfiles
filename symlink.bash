b737676e587f14add920f7e0990af9492ed8c0ef
the_wild_penguin <gaetan.puleo.dev@gmail.com>
2022-08-21 02:05:52 +0200

    feat: update symlinks

--- symlink.bash
+++ symlink.bash
@@ -3,17 +3,17 @@
 case $1 in
   all)
     echo -n "Create symlinks for all directories"
-    stow --verbose --target=$$HOME --restow */
+    stow --verbose --target=$HOME --restow */
     ;;
 
   minimal)
     echo -n "Create symlinks for minimal setup"
-    stow --verbose --target=$$HOME --restow nvim tmux fish
+    stow --verbose --target=$HOME --restow nvim tmux fish
     ;;
 
   delete)
     echo -n "Delete symlinks"
-    stow --verbose --target=$$HOME --delete */
+    stow --verbose --target=$HOME --delete */
     ;;
   
   *)
