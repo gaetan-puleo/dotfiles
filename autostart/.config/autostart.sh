e8254ca961c1aa8fcb0e4d67090448710f574e1b
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2022-08-23 01:27:22 +0200

    fix(dunst): use geometry again

--- autostart/.config/autostart.sh
+++ autostart/.config/autostart.sh
@@ -1,15 +1,16 @@
+
 #auto detect monitors
 autorandr --change & 
 
 bash ~/.fehbg &
 
 # exec_always --no-startup-id feh --bg-fill ~/.config/wallpaper/tokyo-night-girl.jpg
-picom -b --experimental-backends & 
-bash ~/.config/polybar/scripts/launch-bars.sh & 
 
 # keyboard
-
+setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl && xmodmap ~/.Xmodmap &
 
 sxhkd &
-setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl && xmodmap ~/.Xmodmap &
+
+picom -b --experimental-backends & 
+bash ~/.config/polybar/scripts/launch-bars.sh & 
 
