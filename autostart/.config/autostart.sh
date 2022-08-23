a70ccee111b4211cfde952d4412aa46d76cbce92
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2022-08-24 00:46:04 +0200

    feat(nix): configure kitty and firefox

--- autostart/.config/autostart.sh
+++ autostart/.config/autostart.sh
@@ -4,13 +4,14 @@ autorandr --change &
 
 bash ~/.fehbg &
 
+picom -b --experimental-backends & 
+bash ~/.config/polybar/scripts/launch-bars.sh & 
+
+
 # exec_always --no-startup-id feh --bg-fill ~/.config/wallpaper/tokyo-night-girl.jpg
 
 # keyboard
 setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl && xmodmap ~/.Xmodmap &
 
 sxhkd &
-
-picom -b --experimental-backends & 
-bash ~/.config/polybar/scripts/launch-bars.sh & 
 
--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -1,6 +1,53 @@
-{ config, pkgs, ... }:
- 
-{
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.customHomeProfile.GUI;
+  nixGL = (import (pkgs.fetchFromGitHub {
+	owner = "guibou";
+	repo = "nixGL";
+	rev = "7d6bc1b21316bab6cf4a6520c2639a11c25a220e";
+	sha256 = "02y38zmdplk7a9ihsxvnrzhhv7324mmf5g8hmxqizaid5k5ydpr3";
+	
+  }) {}).nixGLDefault;
+in {
+  /* config = lib.mkIf cfg.enable (lib.mkMerge [ */
+    /* (lib.mkIf pkgs.stdenv.isLinux { */
+      home.file = {
+        ".local/share/applications/firefox.desktop" = {
+          text = ''
+            [Desktop Entry]
+            Categories=Network;WebBrowser
+            Exec=nixGL firefox -P Default %U
+            GenericName=Web Browser
+            Icon=${pkgs.firefox}/share/icons/hicolor/128x128/apps/firefox.png
+            MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp
+            Name=Firefox
+            Type=Application
+            Version=1.4
+            Terminal=False
+            Actions=private_window;
+            [Desktop Action private_window]
+            Name=Open a Private Window
+            Icon=${pkgs.firefox}/share/icons/hicolor/128x128/apps/firefox.png
+            Exec=nixGL firefox -P Private %U
+          '';
+          executable = true;
+        };
+      ".local/share/applications/kitty.desktop" = {
+          text = ''
+            [Desktop Entry]
+            Categories=Development;System
+            Exec=nixGL kitty
+            GenericName=Terminal
+            Icon=${pkgs.firefox}/share/icons/hicolor/128x128/apps/firefox.png
+            Name=Kitty
+            Type=Application
+            Version=1.4
+            Terminal=False
+          '';
+          executable = true;
+        };
+      };
+    /* }) */
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "gaetan";
@@ -15,48 +62,91 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "22.05";
+  nixpkgs.config.allowUnfree = true;
  
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
       url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
     }))
+    /* (self: super: { */
+    /*   nix_gl_go = (import (fetchGit { */
+    /*       ref = "main"; */
+    /*       url = "https://github.com/guibou/nixGL.git"; */
+    /*   }) {}).nixGL; */
+    /*   nix_gl = program: pkgs.writeShellScriptBin program.pname '' */
+    /*   ${self.nix_gl_go}/bin/nixGL ${program}/bin/${program.pname} "$@" */
+    /*   ''; */
+    /* }) */
   ];
+
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
   # Let Home Manager install and manage itself.
   # programs.home-manager.enable = true; 
-  home.packages = [
-
+  home.packages = with pkgs; [
+    # Enable opengl
+    nixGL
     # CLI
-    pkgs.bottom
-    pkgs.tmux
-    pkgs.git
-    pkgs.curl
-    pkgs.jq
-    pkgs.httpie
-    pkgs.fzf
-    pkgs.ripgrep
-    pkgs.neofetch
-    pkgs.docker 
-    pkgs.docker-compose
-    pkgs.xclip
-    pkgs.figlet
-    pkgs.lolcat
-    pkgs.stow
-    pkgs.fish
-    pkgs.gcc
-    pkgs.libstdcxx5 
+    bottom
+    tmux
+    git
+    curl
+    jq
+    httpie
+    fzf
+    ripgrep
+    neofetch
+    docker 
+    docker-compose
+    xclip
+    figlet
+    lolcat
+    stow
+    fish
+    gcc
+    libstdcxx5 
  
     # DEV
-    pkgs.neovim
+    neovim
     # pkgs.vimPlugins.packer-nvim
  
     # Node
-    pkgs.nodejs-16_x
-    pkgs.yarn
+    nodejs-16_x
+    yarn
 
     # GUI
-    pkgs.firefox
-    pkgs.polybar
-    pkgs.acpi
+    # pkgs.firefox
+    polybar
+    acpi
+    kitty
 
   ];
+	/*   programs.kitty = { */
+	/*     enable = true; */
+	/*     package = pkgs.writeShellScriptBin "kitty" '' */
+	/* #!/bin/sh */
+	/* ${nixGL}/bin/nixGL ${pkgs.kitty}/bin/kitty "$@" */
+	/*     ''; */
+	/*   }; */
+
+  programs.firefox = {
+    enable = true;
+       profiles.default = {
+      id = 0;
+      name = "Default";
+      isDefault = true;
+    };
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      lastpass-password-manager
+      react-devtools
+      news-feed-eradicator
+      metamask
+      reduxdevtools
+    ];
+  };
 }
