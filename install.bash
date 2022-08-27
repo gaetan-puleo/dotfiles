9273b43e7e206ede6a5b8f77ee2415aecd768a65
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2022-08-27 03:21:20 +0200

    fix: remove useless nixgl

--- install.bash
+++ install.bash
@@ -20,11 +20,6 @@ export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/
 echo "Install home-manager"
 nix-env -iA nixpkgs.home-manager
 
-echo "Install nixGL"
-nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl && nix-channel --update
-
-nix-env -iA nixgl.auto.nixGLDefault   # or replace `nixGLDefault` with your desired wrapper
-
 echo "install stow and git" 
 nix-env -iA nixpkgs.stow
 nix-env -iA nixpkgs.git
