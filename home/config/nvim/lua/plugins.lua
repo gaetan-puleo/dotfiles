36b5bbc1c44918307afbe7ea04b1c553c857aea8
penguin <fake@email.com>
2022-09-22 11:19:07 +0200

    feat(neovim): add editor config

--- home/config/nvim/lua/plugins.lua
+++ home/config/nvim/lua/plugins.lua
@@ -16,13 +16,13 @@ vim.api.nvim_exec([[
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- packager for neovim
   use {'antoinemadec/FixCursorHold.nvim'} -- this plugin needs to be here until this issue is fixed (https://github.com/neovim/neovim/issues/12587)
-  use 'nvim-lua/popup.nvim' 
+  use 'nvim-lua/popup.nvim'
   use 'djoshea/vim-autoread' -- Autoread file
   use 'RishabhRD/popfix'
 
   -- use {
   --   'folke/tokyonight.nvim',
-  --     config = function () 
+  --     config = function ()
   --       require('plugins/tokyonight')
   --     end
   --   ,
@@ -30,7 +30,7 @@ require('packer').startup(function()
   use {
     'folke/tokyonight.nvim',
     config = function ()
-      require('plugins/tokyonight') 
+      require('plugins/tokyonight')
     end
   }
   -- Lua
@@ -43,19 +43,19 @@ require('packer').startup(function()
   }
 
   use {
-    'terrortylor/nvim-comment', 
-    config = function() 
+    'terrortylor/nvim-comment',
+    config = function()
       require('plugins/comment')
     end
   }
 
-  
+
   use 'kyazdani42/nvim-web-devicons'
 
   -- nvim tree
   use ({
     'kyazdani42/nvim-tree.lua',
-    config = function() 
+    config = function()
       local config = require('plugins/nvim-tree')
 
       require('nvim-tree').setup(config)
@@ -64,13 +64,13 @@ require('packer').startup(function()
 
   use {
     'lewis6991/gitsigns.nvim',
-      config = function() 
+      config = function()
         require('plugins/gitsigns')
       end,
   }
-  
+
   -- completion
-  use { 
+  use {
     'hrsh7th/nvim-cmp',
     config = function()
       require('plugins/cmp')
@@ -85,8 +85,10 @@ require('packer').startup(function()
     }
   }
 
-  use { "williamboman/mason.nvim", 
-        config = function() 
+  use { "gpanders/editorconfig.nvim" }
+
+  use { "williamboman/mason.nvim",
+        config = function()
           require('plugins/mason')
         end,
         requires = {
@@ -96,9 +98,9 @@ require('packer').startup(function()
       }
 
   -- lsp
-  use({ 
-    'neovim/nvim-lspconfig', 
-    config = function() 
+  use({
+    'neovim/nvim-lspconfig',
+    config = function()
       --require('plugins/lsp-config')
     end,
   })
@@ -113,7 +115,7 @@ require('packer').startup(function()
 
   -- bufferline
   use({'akinsho/nvim-bufferline.lua',
-    config = function () 
+    config = function ()
       require('plugins/bufferline')
     end
   })
@@ -122,22 +124,22 @@ require('packer').startup(function()
   use('PsychoLlama/further.vim')
 
   -- treesitter
-  use({'nvim-treesitter/nvim-treesitter', 
-    run = ':TSUpdate', 
+  use({'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
 
     requires = {
       'JoosepAlviste/nvim-ts-context-commentstring',
       'windwp/nvim-ts-autotag',
       'nvim-treesitter/nvim-treesitter-refactor'
     },
-    config = function() 
+    config = function()
       require('plugins/treesitter') end,
-    
+
   })
 
   use({
-    'christoomey/vim-tmux-navigator', 
-    config = function() require('plugins/tmux-navigator') end 
+    'christoomey/vim-tmux-navigator',
+    config = function() require('plugins/tmux-navigator') end
   })
 
   -- finder
@@ -147,13 +149,13 @@ require('packer').startup(function()
       require('plugins/telescope')
     end,
     requires = {
-      {'nvim-lua/plenary.nvim'} 
+      {'nvim-lua/plenary.nvim'}
     }
   }
 
   use {
     "ray-x/lsp_signature.nvim",
-    config = function() 
+    config = function()
       require('plugins/lsp-signature')
     end
   }
@@ -168,15 +170,15 @@ require('packer').startup(function()
   })
 
 
-  use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async', 
+  use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async',
     config = function ()
       require('plugins/nvim-ufo')
     end
   }
 
   use {
-      'kosayoda/nvim-lightbulb',
-      config = function () 
+    'kosayoda/nvim-lightbulb',
+      config = function ()
         require('plugins/lsp-lightbulb')
       end
   }
