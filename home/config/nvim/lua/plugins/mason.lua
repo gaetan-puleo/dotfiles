89a0216419f5ae51f1f0a644e1ac812696dd20bd
penguin <fake@email.com>
2022-11-15 00:54:35 +0100

    feat: update config

--- home/config/nvim/lua/plugins/mason.lua
+++ home/config/nvim/lua/plugins/mason.lua
@@ -43,7 +43,7 @@ navic.setup {
         Operator      = " ",
         TypeParameter = " ",
     },
-    highlight = false,
+    highlight = true,
     separator = " > ",
     depth_limit = 0,
     depth_limit_indicator = "..",
