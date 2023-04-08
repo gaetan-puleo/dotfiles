973c6cd67ca000ca194c9848b501718dc3fbea42
Gaetan Puleo <fake@email.com>
2023-04-08 16:36:44 +0200

    feat(vb-ubuntu): add flatpak

--- machines/vbox-ubuntu/scripts/install.bash
+++ machines/vbox-ubuntu/scripts/install.bash
@@ -1,12 +1,37 @@
 #!/bin/bash
 
+# requirement
 sudo apt install curl -y
 
+echo fs.inotify.max_user_watches=66536 | sudo tee -a /etc/sysctl.conf
+
+sudo apt install flatpak
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+# nix
 bash ~/dotfiles/commons/scripts/install.bash
 
 . /home/gaetan/.nix-profile/etc/profile.d/nix.sh
 
-
 home-manager -f ~/dotfiles/machines/vbox-ubuntu/nix/home.nix switch -b override
 
-echo fs.inotify.max_user_watches=66536 | sudo tee -a /etc/sysctl.conf
+# flatpak
+
+# DEV
+flatpak install flathub com.visualstudio.code
+
+# job
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.slack.Slack
+
+# graphic
+flatpak install flathub org.kde.kdenlive
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub org.kde.krita
+
+# browser
+flatpak install flathub org.mozilla.firefox
+flatpak install flathub com.google.Chrome
+flatpak install flathub com.microsoft.Edge
