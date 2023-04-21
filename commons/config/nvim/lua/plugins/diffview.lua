62fae9129675100ddc9dac02a827e440d34f98ff
Gaetan Puleo <fake@email.com>
2023-04-21 15:05:23 +0200

    refactor: remove global config

--- commons/config/nvim/lua/plugins/diffview.lua
+++
@@ -1,8 +0,0 @@
-local map = vim.api.nvim_set_keymap
-
-local actions = require("diffview.actions")
-require("diffview").setup({
-
-})
-
-map('n', '<leader>gd', ':DiffviewOpen<CR>', {})
--- commons/config/nvim/lua/plugins/spectre.lua
+++
@@ -1,9 +0,0 @@
-local map = vim.api.nvim_set_keymap
-
-require('spectre').setup({
-  color_devicons = true,
-})
-
-
-map('n', "<leader>fr", [[<cmd>lua require('spectre').open()<CR>]], { noremap = true, silent = true})
-
