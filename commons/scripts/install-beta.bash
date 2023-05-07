35b0be6aeddfa2cdbc618a624331b9aee750ebbc
Gaetan Puleo <fake@email.com>
2023-05-07 13:01:48 +0200

    fix: test with docker again

--- commons/scripts/install-beta.bash
+++ commons/scripts/install-beta.bash
@@ -1,16 +1,21 @@
 #!/bin/bash
-#requirement curl and uidmap
+#requirement curl uidmap
 
 # get docker
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sh get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
-# dockerd-rootless-setuptool.sh install
+dockerd-rootless-setuptool.sh install
 
 # check if installed
-# docker run hello-world
+docker run hello-world
+
+# Install rootless podman
+# curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/extras/install-podman | sh -s -- --prefix ~/.local
 
 # get distrobox
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh -s -- --next
+
 
 distrobox create arch --image archlinux --yes -v
