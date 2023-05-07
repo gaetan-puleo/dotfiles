a902fd89da38b504c1edfddbaa7aad0bfb20e8b1
Gaetan Puleo <fake@email.com>
2023-05-07 16:52:49 +0200

    fix: add echo messages

--- commons/scripts/install-docker.bash
+++ commons/scripts/install-docker.bash
@@ -6,13 +6,21 @@ echo 'install docker'
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
+
+echo 'Add docker group'
 sudo groupadd docker 2> /dev/null
 sudo usermod -aG docker $USER 2> /dev/null
 
+
+echo 'refresh group'
 newgrp docker
 
+
+echo 'run hello world'
 docker run hello-world
 
+
+echo 'run start service'
 sudo systemctl enable docker.service 2> /dev/null
 sudo systemctl enable containerd.service 2> /dev/null
 
@@ -22,6 +30,11 @@ curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install > get_
 
 sh ./get_distrobox.sh
 
+
+echo 'clean files'
 # clean
 rm ./get-docker.sh
 rm ./get_distrobox.sh
+
+
+echo 'end files'
