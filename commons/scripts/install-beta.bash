17b16157cd262280e173dd15340e6ae5f79e55e3
Gaetan Puleo <fake@email.com>
2023-05-07 07:43:19 +0200

    fix: use systemctl to start docker

--- commons/scripts/install-beta.bash
+++ commons/scripts/install-beta.bash
@@ -4,5 +4,10 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
+systemctl --user start docker
+
+systemctl --user enable docker
+sudo loginctl enable-linger $(whoami)
+
 # get distrobox
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
