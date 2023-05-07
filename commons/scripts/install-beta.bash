2a8743f5784250c0085afb38c5d0c0882ce3a489
Gaetan Puleo <fake@email.com>
2023-05-07 11:08:57 +0200

    fix: comment docker

--- commons/scripts/install-beta.bash
+++ commons/scripts/install-beta.bash
@@ -2,15 +2,15 @@
 #requirement curl and uidmap
 
 # get docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sh get-docker.sh
 
-dockerd-rootless-setuptool.sh install
+# dockerd-rootless-setuptool.sh install
 
 # check if installed
-docker run hello-world
+# docker run hello-world
 
 # get distrobox
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 
-distrobox create arch --image archlinux --yes
+distrobox create arch --image archlinux --yes -v
