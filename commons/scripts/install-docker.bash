20e939dd5f7d3b62e2b786c01d5b51ec0127b858
Gaetan Puleo <fake@email.com>
2023-05-08 19:03:57 +0200

    f

--- commons/scripts/install-docker.bash
+++ commons/scripts/install-docker.bash
@@ -17,15 +17,11 @@ sudo systemctl enable containerd.service 2> /dev/null
 
 echo 'install distrobox'
 
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install > get_distrobox.sh
-
-sh ./get_distrobox.sh --next
-
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh -s -- --next
 
 echo 'clean files'
 # clean
 rm ./get-docker.sh
-rm ./get_distrobox.sh
 
 echo 'end files'
 
