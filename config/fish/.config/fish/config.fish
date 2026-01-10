d6d4f45dc6b949986c2d54e5b0cfcb7f24beb17d
Gaetan Puleo <12990773+gaetan-puleo@users.noreply.github.com>
2026-01-10 13:52:03 +0100

    feat: add fish plugins

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,10 +1,13 @@
+# Config 
+
+set -g fish_greeting ""
+
+# Alias
 alias nuke 'rm -rf'
 alias md 'mkdir -p'
 alias vim 'nvim'
 alias vi 'nvim'
 alias oc 'opencode'
-
-test -e ~/.fzf.fish; and . ~/.fzf.fish
 
 # Source API keys from secure location
 test -e ~/.secrets/api-keys.fish; and source ~/.secrets/api-keys.fish
