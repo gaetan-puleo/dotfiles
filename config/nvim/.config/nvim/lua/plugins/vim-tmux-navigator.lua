a97649575dc76c08d35a80f5c376aed78a18679e
Gaetan Puleo <12990773+gaetan-puleo@users.noreply.github.com>
2026-01-23 10:51:20 +0100

    fix(nvim): switch to vim-kitty-navigator plugin for proper bidirectional navigation

--- config/nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ config/nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -2,13 +2,12 @@ local is_mac = vim.fn.has("macunix") == 1
 local mod = is_mac and "D" or "M"
 
 return {
-    "MunsMan/kitty-navigator.nvim",
-    opts = {
-        keybindings = {
-            left = "<" .. mod .. "-h>",
-            down = "<" .. mod .. "-j>",
-            up = "<" .. mod .. "-k>",
-            right = "<" .. mod .. "-l>",
-        },
+    "knubie/vim-kitty-navigator",
+    build = "cp ./*.py ~/.config/kitty/",
+    keys = {
+        { "<" .. mod .. "-h>", ":KittyNavigateLeft<CR>", mode = "n", desc = "Navigate left", silent = true },
+        { "<" .. mod .. "-j>", ":KittyNavigateDown<CR>", mode = "n", desc = "Navigate down", silent = true },
+        { "<" .. mod .. "-k>", ":KittyNavigateUp<CR>", mode = "n", desc = "Navigate up", silent = true },
+        { "<" .. mod .. "-l>", ":KittyNavigateRight<CR>", mode = "n", desc = "Navigate right", silent = true },
     },
 }
